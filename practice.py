44e72ecb7d8555ff80ce0bd33b1a38a8feedeca1
kvellaidurai <46344094+kvellaidurai@users.noreply.github.com>
2019-06-30 13:52:18 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -203,4 +203,19 @@ else:
             
 
 
-		
+print diamond like pattern with strings
+	   
+	   
+#include <stdio.h>
+
+int main() {
+char str[100];
+int i,len;
+scanf("%s",str);
+len=strlen(str);
+for(i=0;i<len;i++)
+printf("%-*.*s%*s\n",len,len-i,str,len,str+i);
+for(i=0;i<len;i++)
+printf("%-*.*s%*s\n",len,i+1,str,len,str+len-i-1);
+	return 0;
+}
